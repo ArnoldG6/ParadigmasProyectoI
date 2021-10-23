6aa2f0d7c5bf353ab9fbcfd6cd5cb92df6de421c
Miguelgonz98 <mgonzalex236@gmail.com>
2021-10-23 16:49:54 -0600

    Creation Racket file

---
+++ Proyecto1.rkt
@@ -0,0 +1,19 @@
+#lang racket
+;
+; ===============================================
+;;
+(c) 2021
+; version 1.0.0 2021-11-06
+;;
+-----------------------------------------------
+; EIF400 Paradigmas de Programación
+; 2do ciclo 2021, grupo 01
+; Proyecto 1
+;
+; A00148103 González Carvajal, Miguel
+;           González Quesada, Arnoldo
+;           Ocampo Marín, Victor
+;
+; ===============================================
+;
+; Inserte código místico xd
