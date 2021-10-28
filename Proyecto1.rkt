022b4670c735ed0d94f9be0611b67800602c9255
ArnoldG6 <arnoldgq612@gmail.com>
2021-10-28 15:35:04 -0600

    Function generar_individuos created, with it, all the required functions created and tested too

--- Proyecto1.rkt
+++ Proyecto1.rkt
@@ -1,19 +1,50 @@
 #lang racket
 ;
 ; ===============================================
-;;
-(c) 2021
+;
+;(c) 2021
 ; version 1.0.0 2021-11-06
-;;
------------------------------------------------
+;
+;-----------------------------------------------
 ; EIF400 Paradigmas de Programación
 ; 2do ciclo 2021, grupo 01
 ; Proyecto 1
 ;
 ; A00148103 González Carvajal, Miguel
-;           González Quesada, Arnoldo
+; 117620480 González Quesada, Arnoldo
 ;           Ocampo Marín, Victor
 ;
 ; ===============================================
 ;
 ; Inserte código místico xd
+
+(define (alelo_rand)  ;Genera un número random entre 0y 1
+    (+ 0 (random 2)))
+(define generar_individuo_t ;Genera un individuo con una cadena genética de n alelos, usando "tail recursion".
+  (lambda (m n L)
+    (if (= m (+ n 1))
+        L
+        (cons (alelo_rand) (generar_individuo_t(+ m 1) n L))
+    )
+  )
+)
+(define (generar_individuo) ;Genera un individuo de cádena genética con n = 10 alelos por defecto.
+    (generar_individuo_t 0 10 '())
+)
+(generar_individuo)
+
+(define generar_individuos_t ;Genera n individuos de cadena genética de 10 alelos de manera randomizada, usando "tail recursion".
+  (lambda (m n L)
+    (if (= m n)
+        L
+        (cons (generar_individuo) (generar_individuos_t(+ m 1) n L))
+    )
+  )
+)
+(define generar_individuos ;Genera n individuos de cádena genética de 10 alelos de manera randomizada.
+  (lambda (n)
+    (generar_individuos_t 0 n '())
+  )
+)
+
+(generar_individuos 5)
